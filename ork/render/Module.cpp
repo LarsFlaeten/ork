5b0f9587043b963db889fcf5c086034b94668fc7
LarsFlaeten <lars.flaeten@gmail.com>
2016-06-18 00:10:16 +0200

    Update of XXX

--- ork/render/Module.cpp
+++ ork/render/Module.cpp
@@ -1,24 +1,42 @@
 /*
  * Ork: a small object-oriented OpenGL Rendering Kernel.
- * Copyright (c) 2008-2010 INRIA
+ * Website : http://ork.gforge.inria.fr/
+ * Copyright (c) 2008-2015 INRIA - LJK (CNRS - Grenoble University)
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software without 
+ * specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
-
 /*
- * Authors: Eric Bruneton, Antoine Begault, Guillaume Piolat.
+ * Ork is distributed under the BSD3 Licence. 
+ * For any assistance, feedback and remarks, you can check out the 
+ * mailing list on the project page : 
+ * http://ork.gforge.inria.fr/
+ */
+/*
+ * Main authors: Eric Bruneton, Antoine Begault, Guillaume Piolat.
  */
 
 #include "ork/render/Module.h"
@@ -29,6 +47,8 @@
 #include "ork/math/mat2.h"
 #include "ork/resource/ResourceTemplate.h"
 #include "ork/render/FrameBuffer.h"
+
+using namespace std;
 
 namespace ork
 {
@@ -308,7 +328,28 @@ void Module::addFeedbackVarying(const string &name)
 
 void Module::addInitialValue(ptr<Value> value)
 {
-    initialValues.insert(make_pair(value->getName(), value));
+    ptr<ValueSubroutine> sub = value.cast<ValueSubroutine>();
+    if (sub != NULL) {
+        switch (sub->getStage()) {
+        case VERTEX:
+            initialValues.insert(make_pair("VERTEX " + value->getName(), value));
+            break;
+        case TESSELATION_CONTROL:
+            initialValues.insert(make_pair("TESS_CONTROL " + value->getName(), value));
+            break;
+        case TESSELATION_EVALUATION:
+            initialValues.insert(make_pair("TESS_EVAL " + value->getName(), value));
+            break;
+        case GEOMETRY:
+            initialValues.insert(make_pair("GEOMETRY " + value->getName(), value));
+            break;
+        case FRAGMENT:
+            initialValues.insert(make_pair("FRAGMENT " + value->getName(), value));
+            break;
+        }
+    } else {
+        initialValues.insert(make_pair(value->getName(), value));
+    }
 }
 
 void Module::swap(ptr<Module> s)
@@ -332,7 +373,7 @@ void Module::printLog(int shaderId, int nlines, const char** lines, bool error)
 {
     GLint logLength;
     glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &logLength);
-    ptr<Logger> logger = error ? Logger::ERROR_LOGGER : Logger::WARNING_LOGGER;
+    Logger* logger = error ? Logger::ERROR_LOGGER.get() : Logger::WARNING_LOGGER.get();
     if (logger != NULL && logLength > 1) {
         ostringstream msg;
         int l = 1;
@@ -371,7 +412,7 @@ public:
     {
         e = e == NULL ? desc->descriptor : e;
         try {
-            checkParameters(desc, e, "name,version, source,vertex,tessControl,tessEvaluation,geometry,fragment,options,");
+            checkParameters(desc, e, "name,version, source,vertex,tessControl,tessEvaluation,geometry,fragment,feedback,varyings,options,");
 
             int version;
             getIntParameter(desc, e, "version", &version);
@@ -699,7 +740,7 @@ public:
                                 }
                             }
                         }
-                    } else if (strncmp(type.c_str(), "uniformSampler", 14) == 0) {
+                    } else if (strcmp(type.c_str(), "uniformSampler") == 0) {
                         checkParameters(desc, f, "name,texture,");
                         ptr<Texture> t = manager->loadResource(f->Attribute("texture")).cast<Texture>();
                         if (t == NULL) {
@@ -709,6 +750,25 @@ public:
                             throw exception();
                         }
                         v = new ValueSampler(SAMPLER_2D, n, t);
+                    } else if (strcmp(type.c_str(), "uniformSubroutine") == 0) {
+                        checkParameters(desc, f, "name,subroutine,stage,");
+                        assert(f->Attribute("stage") != NULL);
+                        if (strcmp(f->Attribute("stage"), "VERTEX") == 0) {
+                            v = new ValueSubroutine(VERTEX, n, string(f->Attribute("subroutine")));
+                        } else if (strcmp(f->Attribute("stage"), "TESSELATION_CONTROL") == 0) {
+                            v = new ValueSubroutine(TESSELATION_CONTROL, n, string(f->Attribute("subroutine")));
+                        } else if (strcmp(f->Attribute("stage"), "TESSELATION_EVALUATION") == 0) {
+                            v = new ValueSubroutine(TESSELATION_EVALUATION, n, string(f->Attribute("subroutine")));
+                        } else if (strcmp(f->Attribute("stage"), "GEOMETRY") == 0) {
+                            v = new ValueSubroutine(GEOMETRY, n, string(f->Attribute("subroutine")));
+                        } else if (strcmp(f->Attribute("stage"), "FRAGMENT") == 0) {
+                            v = new ValueSubroutine(FRAGMENT, n, string(f->Attribute("subroutine")));
+                        } else {
+                            if (Logger::ERROR_LOGGER != NULL) {
+                                log(Logger::ERROR_LOGGER, desc, e, "Invalid shader stage '" + string(f->Attribute("stage")) + "'");
+                            }
+                            throw exception();
+                        }
                     } else {
                         if (Logger::ERROR_LOGGER != NULL) {
                             log(Logger::ERROR_LOGGER, desc, e, "Unsupported type specifier '" + type + "'");
@@ -772,7 +832,6 @@ public:
                     addFeedbackVarying(varying);
                     start = index + 1;
                 }
-
             }
 
             desc->clearData();
